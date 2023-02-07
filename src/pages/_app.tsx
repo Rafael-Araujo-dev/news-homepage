1ef7e792aaecbbd59a1585ed32d018209b41fbdf
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2023-02-07 09:04:34 -0300

    feat: adiciona react-hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3355535, 1)
+  }, [])
+
   return <Component {...pageProps} />
 }
